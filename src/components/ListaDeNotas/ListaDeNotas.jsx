221500f3b9ad41d5c123e9379f76312896053b76
Victor Kist <victoraugustokist@gmail.com>
2021-04-29 21:24:21 -0300

    ajustes

--- src/components/ListaDeNotas/ListaDeNotas.jsx
+++ src/components/ListaDeNotas/ListaDeNotas.jsx
@@ -9,7 +9,10 @@ class ListaDeNotas extends Component {
         {this.props.notas.map((nota, index) => {
           return (
             <li className="lista-notas_item" key={index}>
-              <CardNota titulo={nota.titulo} texto={nota.texto}/>
+              <CardNota
+              apagarNota={this.props.apagarNota}
+              titulo={nota.titulo}
+              texto={nota.texto}/>
             </li>
           );
         })}
